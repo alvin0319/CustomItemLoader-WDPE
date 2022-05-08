b149f8e08a2a98adf7f050dd526273334eec17f4
alvin0319 <alvin7810@naver.com>
2022-05-08 11:18:49 +0900

    Register ItemComponentPacket to Codec

--- src/main/kotlin/dev/minjae/customitemloader/wdpe/Loader.kt
+++ src/main/kotlin/dev/minjae/customitemloader/wdpe/Loader.kt
@@ -1,7 +1,10 @@
 package dev.minjae.customitemloader.wdpe
 
+import com.nukkitx.protocol.bedrock.packet.ItemComponentPacket
+import com.nukkitx.protocol.bedrock.v419.serializer.ItemComponentSerializer_v419
 import dev.minjae.customitemloader.wdpe.handler.DownstreamPacketHandler
 import dev.waterdog.waterdogpe.event.defaults.InitialServerConnectedEvent
+import dev.waterdog.waterdogpe.event.defaults.ProtocolCodecRegisterEvent
 import dev.waterdog.waterdogpe.network.session.bedrock.BedrockDefaultClient
 import dev.waterdog.waterdogpe.plugin.Plugin
 
@@ -12,6 +15,14 @@ class Loader : Plugin() {
             val player = event.player
             player.pluginDownstreamHandler =
                 DownstreamPacketHandler((player.downstream as BedrockDefaultClient).session.session)
+        }
+
+        proxy.eventManager.subscribe(ProtocolCodecRegisterEvent::class.java) { event ->
+            event.codecBuilder.registerPacket(
+                ItemComponentPacket::class.java,
+                ItemComponentSerializer_v419.INSTANCE,
+                162
+            )
         }
     }
 }
